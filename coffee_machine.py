20cf7743fc34c7f59a5e028b070983515c0969eb
totyai <90376621+totyai@users.noreply.github.com>
2025-12-10 20:38:23 +0000

    - Completed project

--- coffee_machine.py
+++ coffee_machine.py
@@ -2,5 +2,28 @@ from resources.menu import Menu
 from resources.coffee_maker import CoffeeMaker
 from resources.money_machine import MoneyMachine
 
-coffemachine = CoffeeMaker()
-coffemachine.report()
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+
+def main():
+    cont = True
+    while cont:
+        order = input(f"What would you like? ({menu.get_items()}) ").lower()
+        if order == "off":
+            cont = False
+            print("The coffee machine shuts down.")
+        elif order == "report":
+            coffee_maker.report()
+            money_machine.report()
+        else:
+            drink = menu.find_drink(order)
+            if drink:
+                if coffee_maker.is_resource_sufficient(drink):
+                    if money_machine.make_payment(drink.cost):
+                        coffee_maker.make_coffee(drink)
+
+
+if __name__ == "__main__":
+    main()
