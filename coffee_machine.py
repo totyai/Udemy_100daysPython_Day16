0e5140c7a58621712e42dcf4b0fa895e32f7aedf
totyai <90376621+totyai@users.noreply.github.com>
2025-12-10 19:34:41 +0000

    - Added project resources - Added project documentations - Finished Notes

---
+++ coffee_machine.py
@@ -0,0 +1,6 @@
+from resources.menu import Menu
+from resources.coffee_maker import CoffeeMaker
+from resources.money_machine import MoneyMachine
+
+coffemachine = CoffeeMaker()
+coffemachine.report()
